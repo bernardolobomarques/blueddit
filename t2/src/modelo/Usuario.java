28154c32aaa9312d6d29a3ce45d20bba1e172912
Bernardo Moreira Guimarães Gonçalves <141369080+BernardoMGG@users.noreply.github.com>
2025-06-08 14:52:02 -0300

    Add files via upload

---
+++ t2/src/modelo/Usuario.java
@@ -0,0 +1,46 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+    private String nome;
+    private List<Post> posts;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+        this.posts = new ArrayList<>();
+    }
+
+    public Post criarPost(Usuario usuario, String dataPublicada, String descricao, int upvote, int downvote, Comentario comentario) {
+        Post novoPost = new Post(usuario, dataPublicada, descricao, 0, 0, comentario);
+        posts.add(novoPost);
+        return novoPost;
+    }
+
+    public Comentario comentar(Post post, String texto) {
+        Comentario novoComentario = new Comentario(texto, this, post);
+        post.adicionarComentario(novoComentario);
+        return novoComentario;
+    }
+
+    public Post upvotePost(Post post) {
+        post.setUpvote(post.getUpvote() + 1);
+        return post;
+    }
+
+    public Post downvotePost(Post post) {
+        post.setDownvote(post.getDownvote() + 1);
+        return post;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
+
+
+
+
+
